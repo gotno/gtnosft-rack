8a6cd93510cf6779fe80ef86793c0b6efab8cf91
will floyd <floyd.will@gmail.com>
2024-11-30 14:07:45 -0800

    Add Render module

--- src/Render.cpp
+++ src/Render.cpp
@@ -25,15 +25,21 @@ struct Render : Module {
 
 
 struct RenderWidget : ModuleWidget {
+  int steps = 0;
+
 	RenderWidget(Render* module) {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Render.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		/* addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0))); */
+		/* addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0))); */
+		/* addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH))); */
+		/* addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH))); */
 	}
+
+  void step() override {
+    DEBUG("step %d", ++steps);
+  }
 };
 
 
