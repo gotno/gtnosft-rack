25ed70d0623b6a38c767a57277374796931e52de
will floyd <floyd.will@gmail.com>
2024-12-02 08:52:10 -0800

    menu wip

--- src/Render.cpp
+++ src/Render.cpp
@@ -23,9 +23,8 @@ struct Render : Module {
 	}
 };
 
-
 struct RenderWidget : ModuleWidget {
-  int steps = 0;
+  std::map<std::string, rack::widget::FramebufferWidget*> framebuffers;
 
 	RenderWidget(Render* module) {
 		setModule(module);
@@ -37,8 +36,56 @@ struct RenderWidget : ModuleWidget {
 		/* addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH))); */
 	}
 
-  void step() override {
-    DEBUG("step %d", ++steps);
+  virtual void step() override {
+    ModuleWidget::step();
+  }
+
+  void refreshFramebuffers() {
+    framebuffers.clear();
+
+    for (int64_t& moduleId: APP->engine->getModuleIds()) {
+      rack::app::ModuleWidget* mw = APP->scene->rack->getModule(moduleId);
+
+      rack::widget::Widget* widget;
+      rack::widget::FramebufferWidget* fb;
+
+      if ((widget = dynamic_cast<rack::widget::Widget*>(mw->children.front()))) {
+        if ((fb = dynamic_cast<rack::widget::FramebufferWidget*>(widget->children.front()))) {
+          std::string key = "";
+          key.append(mw->getModule()->getModel()->plugin->slug.c_str());
+          key.append("/");
+          key.append(mw->getModule()->getModel()->name.c_str());
+          key.append("-panel");
+          framebuffers.emplace(key, fb);
+        }
+      }
+      // if (panel) {
+      // } else if (widget) {
+      //   std::string key = "";
+      //   key.append(mw->getModule()->getModel()->plugin->slug.c_str());
+      //   key.append("/");
+      //   key.append(mw->getModule()->getModel()->name.c_str());
+      //   key.append("-panel (Widget)");
+      // }
+    }
+  }
+
+  void appendContextMenu(Menu* menu) override {
+    menu->addChild(new MenuSeparator);
+    menu->addChild(createMenuItem("Refresh framebuffers", "", [=]() {
+      refreshFramebuffers();
+    }));
+
+    if (framebuffers.size() > 0) {
+      menu->addChild(new rack::ui::MenuSeparator);
+      menu->addChild(createSubmenuItem("framebuffers", "",
+        [=](Menu* menu) {
+          for (std::pair<std::string, rack::widget::FramebufferWidget*> pair : framebuffers) {
+            menu->addChild(createMenuItem(pair.first.c_str(), "", [=]() {}));
+          }
+        }
+      ));
+    }
   }
 };
 
