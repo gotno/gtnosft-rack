16c061132b603b357b005fae40d5278a1b124c58
Will Floyd <floyd.will@gmail.com>
2025-01-15 21:09:07 -0800

    working chunked image compression

--- src/osc/ChunkedSend/ChunkedImage.cpp
+++ src/osc/ChunkedSend/ChunkedImage.cpp
@@ -2,11 +2,35 @@
 
 #include "../MessagePacker/ImageChunkPacker.hpp"
 
+#define QOI_IMPLEMENTATION
+#include "../../dep/qoi/qoi.h"
+
 ChunkedImage::ChunkedImage(uint8_t* _pixels, int32_t _width, int32_t _height):
   ChunkedSend(_pixels, _width * _height * ChunkedImage::DEPTH),
   width(_width), height(_height) {
     chunkSize = MAX_CHUNK_SIZE - 1024;
     // chunkSize = 1024 * 32;
+
+    qoi_desc desc;
+    desc.width = width;
+    desc.height = height;
+    desc.channels = ChunkedImage::DEPTH;
+    desc.colorspace = 0;
+
+    int compressedLength{0};
+    void* compressedData = qoi_encode(data, &desc, &compressedLength);
+    INFO("image %d bytes, %d compressed", width * height * ChunkedImage::DEPTH, compressedLength);
+
+    if (compressedData) {
+      delete[] data;
+      data = new uint8_t[compressedLength];
+      size = compressedLength;
+      memcpy(data, compressedData, compressedLength);
+      free(compressedData);
+    } else {
+      WARN("unable to compress image data!");
+    }
+
     init();
   }
 
