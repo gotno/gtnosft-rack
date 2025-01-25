2f65d418434e160cef9272207ae5a0adf3a5d558
Will Floyd <floyd.will@gmail.com>
2025-01-25 10:03:36 -0800

    bump ChunkedImage chunksize from 16k to 32k

--- src/osc/ChunkedSend/ChunkedImage.cpp
+++ src/osc/ChunkedSend/ChunkedImage.cpp
@@ -9,7 +9,7 @@ ChunkedImage::ChunkedImage(uint8_t* _pixels, int32_t _width, int32_t _height):
   ChunkedSend(_pixels, _width * _height * ChunkedImage::DEPTH),
   width(_width), height(_height) {
     // chunkSize = MAX_CHUNK_SIZE - 1024;
-    chunkSize = 1024 * 16;
+    chunkSize = 1024 * 32;
     calculateNumChunks();
   }
 
