82dfdee70a2ac3fd24c130108535e5400e227ee5
Will Floyd <floyd.will@gmail.com>
2025-03-20 17:46:02 -0700

    actually don't use the same port lol

--- src/osc/OscReceiver.hpp
+++ src/osc/OscReceiver.hpp
@@ -7,7 +7,7 @@
 #include "oscpack/osc/OscPacketListener.h"
 #include "oscpack/osc/OscReceivedElements.h"
 
-#define RX_PORT 7746 // RSIM
+#define RX_PORT 7225 // RACK
 
 class RenderWidget;
 class OscSender;
