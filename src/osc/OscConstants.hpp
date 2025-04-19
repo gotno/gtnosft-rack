52320f7da6d6b0bfe830d91416a80621b46a28eb
Will Floyd <floyd.will@gmail.com>
2025-04-19 09:34:52 -0700

    update max msg size, add empty bundle size consts

--- src/osc/OscConstants.hpp
+++ src/osc/OscConstants.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#define MSG_BUFFER_SIZE 1280 // safely under 1500 octect MTU?
+#define MSG_BUFFER_SIZE 1452 // best case, 1232 worst case
+// according to https://io7m.com/documents/udp-reliable/
+
+#define EMPTY_BUNDLE_SIZE 16
 #define TX_PORT 7746 // RSIM
 #define RX_PORT 7225 // RACK
 #define HEARTBEAT_DELAY 1000 // ms between heartbeats
