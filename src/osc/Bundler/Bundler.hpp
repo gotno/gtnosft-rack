095b7a94bd86c0110a87f7fb5faf4d24c8c33519
Will Floyd <floyd.will@gmail.com>
2025-04-19 09:19:22 -0700

    introducing Bundler, the new MessagePacker
    Bundler serves the same purpose as MessagePacker, but with a few
    improvements:
    1. it is built to support adding multiple messages to a bundle, instead
       of the hacky way we were doing it with, for example, the module stubs
       packer.
    2. it handles buffer overruns, allowing splitting large amounts of data
       into multiple bundles
    3. it gracefully handles single messages that won't fit in a bundle. it
       does this by skipping them for now. perhaps in the future we could
       try progressively truncating any long strings or even splitting known
       problem messages into multiple messages.
    4. it no longer needs an override of the noop method. noop is simply a
       Bundler with an empty messages vector

---
+++ src/osc/Bundler/Bundler.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "rack.hpp"
+
+#include <string>
+#include <vector>
+#include <functional>
+
+#include "oscpack/osc/OscOutboundPacketStream.h"
+#include "oscpack/osc/OscTypes.h"
+
+struct Bundler {
+  Bundler(): name("base bundler (noop)") {
+    // test too many messages for one packet (multiple sends)
+    // for (int32_t i = 0; i < 200; ++i) {
+    //   messages.emplace_back("/test", [i](osc::OutboundPacketStream& pstream) {
+    //     pstream << i;
+    //   });
+    // }
+
+    // test single message too large for packet (skip message)
+    // messages.emplace_back("/test", [](osc::OutboundPacketStream& pstream) {
+    //   for (int32_t i = 0; i < 25000; ++i) {
+    //     pstream << i;
+    //   }
+    // });
+  }
+  virtual ~Bundler() {}
+
+  std::string name{""};
+  std::string getNextPath() {
+    if (!hasRemainingMessages()) return "";
+    return messages[messageCursor].first;
+  }
+
+  bool isNoop() { return messages.empty(); }
+  bool hasRemainingMessages() { return messageCursor < messages.size(); }
+  void advance() { ++messageCursor; }
+
+  void bundle(osc::OutboundPacketStream& pstream) {
+    while(hasRemainingMessages()) {
+      try {
+        buildNextMessage(pstream);
+      } catch (osc::OutOfBufferMemoryException& e) {
+        DEBUG(
+          "out of memory while building %lld:%s",
+          messageCursor,
+          getNextPath().c_str()
+        );
+        return;
+      }
+    }
+  }
+
+  virtual void finish() {}
+  // TODO: do we need this with the smaller packet sizes? 
+  int32_t postSendDelayMs{0};
+
+private:
+  typedef std::function<void(osc::OutboundPacketStream&)> messageBuilder;
+  typedef std::pair<std::string /* path */, messageBuilder> message;
+  std::vector<message> messages;
+  size_t messageCursor{0};
+
+  void buildNextMessage(osc::OutboundPacketStream& pstream) {
+    message& msg = messages[messageCursor];
+    pstream << osc::BeginMessage(msg.first.c_str());
+    msg.second(pstream);
+    pstream << osc::EndMessage;
+    advance();
+  }
+};
