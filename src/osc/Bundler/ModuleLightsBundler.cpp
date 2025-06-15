2cdc117ebef9de4a67165d365b1fec5732e946ab
Will Floyd <floyd.will@gmail.com>
2025-06-14 17:51:47 -0700

    implement light state bundling

--- src/osc/Bundler/ModuleLightsBundler.cpp
+++ src/osc/Bundler/ModuleLightsBundler.cpp
@@ -8,6 +8,7 @@ ModuleLightsBundler::ModuleLightsBundler(
 ): Bundler("ModuleLightsBundler") {
   beforeDestroy = callback;
 
+  // filter subscriptions to loaded modules
   std::vector<int64_t> rackModuleIds = APP->engine->getModuleIds();
   std::sort(rackModuleIds.begin(), rackModuleIds.end());
 
@@ -21,16 +22,72 @@ ModuleLightsBundler::ModuleLightsBundler(
   );
   moduleIds.resize(it - moduleIds.begin());
 
-  for (const auto& id : moduleIds) {
-    messages.emplace_back(
-      "/set/state/module/light",
-      [id](osc::OutboundPacketStream& pstream) {
-        INFO("bundling lights for module %lld", id);
-        // pstream << id
-        //   << pluginSlug.c_str()
-        //   << moduleSlug.c_str()
-        //   ;
+  for (const auto& moduleId : moduleIds) {
+    if (lights.count(moduleId) == 0) {
+      collectLights(moduleId);
+      continue;
+    }
+
+    auto& lightList = lights.at(moduleId);
+
+    for (auto it = lightList.begin(); it != lightList.end(); ++it) {
+      rack::app::LightWidget* widget = it->first;
+      auto& state = it->second;
+
+      if (state.changed(widget)) {
+        state.update(widget);
+        addMessage(moduleId, state);
       }
-    );
+    }
   }
+}
+
+void ModuleLightsBundler::collectLights(int64_t moduleId) {
+  INFO("bundler collect");
+  using namespace rack::app;
+  using namespace rack::widget;
+
+  ModuleWidget* moduleWidget = APP->scene->rack->getModule(moduleId);
+  int lightId = 0;
+  lights.emplace(moduleId, LightList());
+  auto& lightList = lights.at(moduleId);
+
+  // panel lights
+  for (Widget* widget : moduleWidget->children) {
+    if (LightWidget* lightWidget = dynamic_cast<LightWidget*>(widget)) {
+      lightList.emplace_back(lightWidget, LightState(lightId, lightWidget));
+      addMessage(moduleId, lightList.back().second);
+      ++lightId;
+    }
+  }
+
+  // param lights
+  for (ParamWidget* & paramWidget : moduleWidget->getParams()) {
+    for (Widget* & widget : paramWidget->children) {
+      if (LightWidget* lightWidget = dynamic_cast<LightWidget*>(widget)) {
+        lightList.emplace_back(lightWidget, LightState(lightId, lightWidget));
+        addMessage(moduleId, lightList.back().second);
+        ++lightId;
+      }
+    }
+  }
+}
+
+void ModuleLightsBundler::addMessage(
+  int64_t moduleId,
+  const LightState& state
+) {
+  messages.emplace_back(
+    "/set/state/module/light",
+    [moduleId, state](osc::OutboundPacketStream& pstream) {
+      pstream << moduleId
+        << state.id
+        << state.visible
+        << state.color.r
+        << state.color.g
+        << state.color.b
+        << state.color.a
+        ;
+    }
+  );
 }
--- src/osc/Bundler/ModuleLightsBundler.hpp
+++ src/osc/Bundler/ModuleLightsBundler.hpp
@@ -2,9 +2,40 @@
 
 #include "Bundler.hpp"
 
+struct ComponentState{
+  ComponentState(int _id, bool _visible): id(_id), visible(_visible) {}
+
+  int id;
+  bool visible;
+};
+
+struct LightState: public ComponentState {
+  LightState(int _id, rack::app::LightWidget* widget):
+    ComponentState(_id, widget->isVisible()), color(widget->color) {}
+
+  NVGcolor color;
+
+  bool changed(rack::app::LightWidget* widget) {
+    if (widget->isVisible() != visible) return true;
+    if (!rack::color::isEqual(widget->color, color)) return true;
+    return false;
+  }
+
+  void update(rack::app::LightWidget* widget) {
+    visible = widget->isVisible();
+    color = widget->color;
+  }
+};
+
 struct ModuleLightsBundler : Bundler {
+  typedef std::list<std::pair<rack::app::LightWidget*, LightState>> LightList;
+  inline static std::map<int64_t, LightList> lights;
+
   ModuleLightsBundler(
     const std::vector<int64_t>& moduleIds,
     std::function<void()> callback
   );
+
+  void collectLights(int64_t moduleId);
+  void addMessage(int64_t moduleId, const LightState& state);
 };
