149ada65ef24743ec6467adbf0cc9ffd182da8b9
Will Floyd <floyd.will@gmail.com>
2025-08-04 08:49:17 -0700

    add /add/cable route

--- src/osc/Bundler/CablesBundler.cpp
+++ src/osc/Bundler/CablesBundler.cpp
@@ -1,36 +1,43 @@
 #include "CablesBundler.hpp"
 
+CablesBundler::CablesBundler(
+  int64_t cableId,
+  int32_t returnId
+) : Bundler("CablesBundler") {
+  bundleCable(cableId, returnId);
+}
+
 CablesBundler::CablesBundler(): Bundler("CablesBundler") {
   std::vector<int64_t> cableIds = APP->engine->getCableIds();
 
-  // messages.emplace_back(
-  // "/set/cables/overview",
-  //   [&](osc::OutboundPacketStream& pstream) {
-  //     pstream << (int32_t)cableIds.size();
-  //   }
-  // );
-
   for (int64_t cableId : cableIds) {
-    rack::engine::Cable* cable = APP->engine->getCable(cableId);
-    rack::app::CableWidget* cableWidget = APP->scene->rack->getCable(cableId);
-
-    int64_t inputModuleId = cable->inputModule->getId();
-    int64_t outputModuleId = cable->outputModule->getId();
-    int32_t inputId = cable->inputId;
-    int32_t outputId = cable->outputId;
-    std::string color = rack::color::toHexString(cableWidget->color);
-
-    messages.emplace_back(
-      "/set/cable",
-      [=](osc::OutboundPacketStream& pstream) {
-        pstream << cableId
-          << inputModuleId
-          << outputModuleId
-          << inputId
-          << outputId
-          << color.c_str()
-          ;
-      }
-    );
+    bundleCable(cableId);
   }
+
+  // TODO: send "finished" message with total sent count
+}
+
+void CablesBundler::bundleCable(int64_t cableId, int32_t returnId) {
+  rack::engine::Cable* cable = APP->engine->getCable(cableId);
+  rack::app::CableWidget* cableWidget = APP->scene->rack->getCable(cableId);
+
+  int64_t inputModuleId = cable->inputModule->getId();
+  int64_t outputModuleId = cable->outputModule->getId();
+  int32_t inputId = cable->inputId;
+  int32_t outputId = cable->outputId;
+  std::string color = rack::color::toHexString(cableWidget->color);
+
+  messages.emplace_back(
+    "/set/cable",
+    [=](osc::OutboundPacketStream& pstream) {
+      pstream << cableId
+        << inputModuleId
+        << outputModuleId
+        << inputId
+        << outputId
+        << color.c_str()
+        << returnId
+        ;
+    }
+  );
 }
